82741d586405e39085227287bb0d187ca2d970ec
Gianmarcos <gians96@gmail.com>
2025-02-03 15:29:42 -0500

    [ADD] controller auth ms

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) { }
+
+  @Get('/')
+  getUsers() {
+    return 'getUser'
+  }
+
+  @Post('register')
+  register(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto);
+  }
+
+  @Post('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto);
+
+  }
+
+  @Post('verify-token')
+  verifyToken() {
+    return this.client.send('auth.verify.token.user', {});
+
+  }
 }
